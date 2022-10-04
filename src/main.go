fbebfbb7b58403b6bfa68c07a5a53ee9a0ccdf99
luuhai48 <luuhai.hn48@gmail.com>
2022-10-04 14:29:44 +0700

    Try graceful shutdown

--- src/main.go
+++ src/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -15,7 +19,26 @@ func init() {
 }
 
 func main() {
-	if err := NewCli().Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := NewCli().RunContext(ctx, os.Args); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("Shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Server exiting")
 }
